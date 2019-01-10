33324dd2036770e3488a4b6e8e60015617730323
DukeValentine <humanix@posteo.de>
2019-01-10 02:14:27 -0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ from nhentai_downloader import __version__,__author__,__email__
 
 with open("requirements.txt") as file:
     requirements = file.read().splitlines()
+    
+with open("README.md", encoding='utf-8') as f:
+    long_description = f.read()
 
 
 setup(
@@ -18,6 +21,10 @@ setup(
      zip_safe=False,
     install_requires=requirements,
     license = 'Apache',
+    url = 'https://gitlab.com/DukeValentine/nhentai-downloader',
+    download_url = 'https://gitlab.com/DukeValentine/nhentai-downloader/-/archive/master/nhentai-downloader-master.tar.gz',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     
     entry_points={
         'console_scripts': [
