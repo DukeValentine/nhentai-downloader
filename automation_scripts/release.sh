19a2d23db5e06b3ffe7df73d8e3c3d04ef06e1ec
DukeValentine <humanix@posteo.de>
2019-03-22 16:32:13 -0300

    Version 0.5.0.1

--- automation_scripts/release.sh
+++ automation_scripts/release.sh
@@ -1,1 +1,3 @@
-curl --header 'Content-Type: application/json' --header 'PRIVATE-TOKEN: $PRIVATE_TOKEN' --data '{  'name': '$CI_COMMIT_TITLE', 'tag_name': '$CI_COMMIT_TAG', 'description': '### $BINARY_MARKDOWN' }' --request POST $CI_API_V4_URL/projects/$CI_PROJECT_ID/releases
+BINARY_MARKDOWN=$(curl --request POST --header "PRIVATE-TOKEN:$PRIVATE_TOKEN" --form "file=@./dist/$BINARY_NAME" $CI_API_V4_URL/projects/$CI_PROJECT_ID/uploads | jq -r '.markdown')
+
+curl --header 'Content-Type: application/json' --header 'PRIVATE-TOKEN: $PRIVATE_TOKEN' --data '{  "name": "$CI_COMMIT_TITLE", "tag_name": "$CI_COMMIT_TAG", "description": "### $BINARY_MARKDOWN" }' --request POST $CI_API_V4_URL/projects/$CI_PROJECT_ID/releases
