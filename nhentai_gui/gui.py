bd1a51bdb1e4b89e2f0848e1912042b25b6e0fe6
DukeValentine <humanix@posteo.de>
2019-08-08 22:32:51 -0300

    Get checked boxes and set text of save location in the gui

--- nhentai_gui/gui.py
+++ nhentai_gui/gui.py
@@ -1,6 +1,6 @@
 from PyQt5.QtCore import Qt , pyqtSlot
 from PyQt5.QtGui import QPalette, QColor
-from PyQt5.QtWidgets import QApplication, QPushButton, QLabel, QWidget,QMessageBox,QMainWindow,QAction, QMenu,QTableWidget,QTableWidgetItem, QFileDialog,QDialog,QCheckBox,QHeaderView
+from PyQt5.QtWidgets import QApplication, QPushButton, QLabel, QWidget,QMessageBox,QMainWindow,QAction, QMenu,QTableWidget,QTableWidgetItem, QFileDialog,QDialog,QCheckBox,QHeaderView, QLineEdit
 import sys
 from platform import system
 from PyQt5 import uic
@@ -12,19 +12,33 @@ tag_dialog_class = uic.loadUiType("tags_selection.ui")[0]
 
 ALL_LANGUAGES = ["english","japanese","chinese","translated"]
 
+COMMON_TAGS = ["incest","lolicon","ahegao","shotacon","sweat","blowjob","nakadashi","impregnation","dark skin","footjob","harem","stockings","paizuri"]
+
+
+
+class tagCheckBox(QCheckBox):
+    def __init__(self):
+        return
+    
+
 
 
 class TagDialog(QDialog, tag_dialog_class):
+    
+    
     def __init__(self, parent=None,columns = 8, languages = ALL_LANGUAGES):
         QMainWindow.__init__(self, parent)
         self.setupUi(self)
+        
+        
+        self.buttonBox.accepted.connect(self.retrieve_checked_cells)
         
         
         self.tableWidget.horizontalHeader().hide()
         self.tableWidget.verticalHeader().hide()
         
         self.tableWidget.setColumnCount(columns)
-        self.tableWidget.setRowCount(2)
+        self.tableWidget.setRowCount(5)
         
         self.tableWidget.setSpan(0,0,1,8)
         self.tableWidget.setSpan(1,len(languages), 1, columns - len(languages))
@@ -33,10 +47,40 @@ class TagDialog(QDialog, tag_dialog_class):
         language_item.setTextAlignment(Qt.AlignHCenter)
         self.tableWidget.setItem(0,0,language_item)
         
+        self.tableWidget.setSpan(2,0,1,8)
+        tag_item = QTableWidgetItem("Common tags")
+        tag_item.setTextAlignment(Qt.AlignHCenter)
+        self.tableWidget.setItem(2,0,tag_item)
+        
+        
         self.tableWidget.horizontalHeader().setSectionResizeMode(1, QHeaderView.ResizeToContents)
         
         for column,language in enumerate(languages,0):
             self.tableWidget.setCellWidget(1,column, QCheckBox(language))
+            
+        for index,tag in enumerate(COMMON_TAGS,0):
+           self.tableWidget.setCellWidget(3 + index/columns ,index%columns, QCheckBox(tag))
+            
+    
+    def retrieve_checked_cells(self):
+        checked_cells = []
+        
+        for row in range(self.tableWidget.rowCount()):
+            for column in range(self.tableWidget.columnCount()):
+                try:
+                    if(self.tableWidget.cellWidget(row,column).isChecked()):
+                        checked_cells.append(self.tableWidget.cellWidget(row,column).text())
+                        
+                except AttributeError:
+                    pass
+                    
+                
+        
+        
+        
+        
+        print(checked_cells)
+        
         
         
         
@@ -63,12 +107,10 @@ class MyWindowClass(QMainWindow, form_class):
         tag_dialog.exec_()
         
         
-        
     def location_selection_click(self):
         options = QFileDialog.Options()
-        fileName = QFileDialog.getExistingDirectory(self,"Select save location", "", options=options)
-        if fileName:
-            print(fileName)
+        directory = QFileDialog.getExistingDirectory(self,"Select save location", "", options=QFileDialog.ShowDirsOnly)#
+        self.location_directory.setText(directory)
             
 
             
